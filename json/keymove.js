8d825c7b10798b540a1e0287eb11340f7474917f
stevenribette <steven.ribette@epsi.fr>
2019-10-15 10:45:16 +0200

    spotlight,  keymoves, news

---
+++ json/keymove.js
@@ -0,0 +1,151 @@
+var keymove = [
+    {
+        "Character" : "akuma",
+        "Content" : {
+            "Punish" : [
+                {
+                    "command" : "1",
+                    "frame" : "10"
+                }
+            ],
+            "Good moves" : [],
+            "Combos" : []
+        }
+    },
+    {
+        "Character" : "alisa"
+    },
+    {
+        "Character" : "anna"
+    },
+    {
+        "Character" : "armor_king"
+    },
+    {
+        "Character" : "asuka"
+    },
+    {
+        "Character" : "bob"
+    },
+    {
+        "Character" : "bryan"
+    },
+    {
+        "Character" : "claudio"
+    },
+    {
+        "Character" : "devil_jin"
+    },
+    {
+        "Character" : "dragunov"
+    },
+    {
+        "Character" : "eddy"
+    },
+    {
+        "Character" : "eliza"
+    }
+    ,
+    {
+        "Character" : "feng"        
+    },
+    {
+        "Character" : "geese"
+    },
+    {
+        "Character" : "gigas"
+    },
+    {
+        "Character" : "heihachi"
+    },
+    {
+        "Character" : "hwoarang"
+    },
+    {
+        "Character" : "jack7"
+    },
+    {
+        "Character" : "jin"
+    },
+    {
+        "Character" : "josie"
+    },
+    {
+        "Character" : "julia"
+    },
+    {
+        "Character" : "katarina"        
+    },
+    {
+        "Character" : "kazumi"
+    },
+    {
+        "Character" : "kazuya"
+    },
+    {
+        "Character" : "king"
+    },
+    {
+        "Character" : "kuma"        
+    },
+    {
+        "Character" : "lars"
+    },
+    {
+        "Character" : "law"
+    },
+    {
+        "Character" : "lee"
+    },
+    {
+        "Character" : "lei"
+    },
+    {
+        "Character" : "leo"
+    },
+    {
+        "Character" : "lili"        
+    },
+    {
+        "Character" : "lucky_chloe"
+    },
+    {
+        "Character" : "marduk"        
+    },
+    {
+        "Character" : "master_raven"
+    },
+    {
+        "Character" : "miguel"
+    },
+    {
+        "Character" : "negan"
+    },
+    {
+        "Character" : "nina"        
+    },
+    {
+        "Character" : "noctis"        
+    },
+    {
+        "Character" : "panda"
+    },
+    {
+        "Character" : "paul"        
+    },
+    {
+        "Character" : "shaheen"
+    },
+    {
+        "Character" : "steve"
+    },
+    {
+        "Character" : "xiaoyu"
+    },
+    {
+        "Character" : "yoshimitsu"
+    },
+    {
+        "Character" : "zafina"
+    }
+]
---
+++ json/spotlight.js
@@ -0,0 +1,328 @@
+var spotlight = [
+    {
+        "Character" : "akuma",
+        "Content" : [
+            {
+                "Sponsor" : "CRaZY",
+                "Player" : "Super Akouma",
+                "Country" : "France",
+                "Desc" : "TEKKEN 7 European Champion 🏆 2 time Tekken World Tour Finalist and World Traveler",
+                "Link" : "SuperAkouma"
+            },
+            {
+                "Sponsor" : "RIZE",
+                "Player" : "Gouki Akuma",
+                "Country" : "England",
+                "Desc" : "Professional T̶e̶k̶k̶e̶n̶ ̶7̶ Akuma player and combo connoisseur from the UK. Currently representing RIZE.",
+                "Link" : "GoukiAkuma_"
+            }
+        ]
+    },
+    {
+        "Character" : "alisa",
+        "Content" : [
+            {
+                "Sponsor" : "Rox Dragon",
+                "Player" : "Chanel",
+                "Country" : "Korea",
+                "Desc" : "🐲「 ROX_Dragons 」CHANEL 🐲",
+                "Link" : "tekkenchanel"
+            }
+        ]        
+    },
+    {
+        "Character" : "anna"
+    },
+    {
+        "Character" : "armor_king"
+    },
+    {
+        "Character" : "asuka"
+    },
+    {
+        "Character" : "bob"
+    },
+    {
+        "Character" : "bryan",
+        "Content" : [
+            {
+                "Sponsor" : "Rox Dragon",
+                "Player" : "Knee",
+                "Country" : "Korea",
+                "Desc" : "안녕하세요. ROX Gaming 소속 철권 프로게이머 무릎입니다! 자유로운 트윗 하고 있습니다 :)",
+                "Link" : "holyknee"
+            }
+        ]        
+    },
+    {
+        "Character" : "claudio",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Jopelix",
+                "Country" : "",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "devil_jin",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Qudans",
+                "Country" : "Korea",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "dragunov",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "NOBI",
+                "Country" : "Japan",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "eddy"
+    },
+    {
+        "Character" : "eliza",
+        "Content" : [
+            {
+                "Sponsor" : "RIZE",
+                "Player" : "Eminor",
+                "Country" : "England",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    }
+    ,
+    {
+        "Character" : "feng"        
+    },
+    {
+        "Character" : "geese"
+    },
+    {
+        "Character" : "gigas"
+    },
+    {
+        "Character" : "heihachi",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "JDCR",
+                "Country" : "Korea",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "hwoarang",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "SpeedKicks",
+                "Country" : "USA",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "jack7",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Gunni",
+                "Country" : "France",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "jin",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "CBM",
+                "Country" : "Korea",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "josie",
+        "Content" : [
+            {
+                "Sponsor" : "RIZE",
+                "Player" : "ChickenMaru",
+                "Country" : "England",
+                "Desc" : "Law graduate | Pro Tekken player (Josie main) | Anime fan (not a weeb) | Proudly sponsored by @RizeGamingPro",
+                "Link" : "chickenmarutk"
+            }
+        ]
+    },
+    {
+        "Character" : "julia"
+    },
+    {
+        "Character" : "katarina"        
+    },
+    {
+        "Character" : "kazumi",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Arslan Ash",
+                "Country" : "Pakistan",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "kazuya"
+    },
+    {
+        "Character" : "king",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Lil Majin",
+                "Country" : "USA",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "kuma"        
+    },
+    {
+        "Character" : "lars"
+    },
+    {
+        "Character" : "law"
+    },
+    {
+        "Character" : "lee"
+    },
+    {
+        "Character" : "lei"
+    },
+    {
+        "Character" : "leo"
+    },
+    {
+        "Character" : "lili"        
+    },
+    {
+        "Character" : "lucky_chloe"
+    },
+    {
+        "Character" : "marduk"        
+    },
+    {
+        "Character" : "master_raven",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Tissuemon",
+                "Country" : "Italia",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "miguel",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Sephiblack",
+                "Country" : "Germany",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "negan",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Elix",
+                "Country" : "France",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "nina"        
+    },
+    {
+        "Character" : "noctis"        
+    },
+    {
+        "Character" : "panda",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Rangchu",
+                "Country" : "Korea",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "paul"        
+    },
+    {
+        "Character" : "shaheen"
+    },
+    {
+        "Character" : "steve",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "Doug From Paris",
+                "Country" : "France",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "xiaoyu"
+    },
+    {
+        "Character" : "yoshimitsu",
+        "Content" : [
+            {
+                "Sponsor" : "",
+                "Player" : "EyeMusician",
+                "Country" : "",
+                "Desc" : "",
+                "Link" : ""
+            }
+        ]
+    },
+    {
+        "Character" : "zafina"
+    }
+]
